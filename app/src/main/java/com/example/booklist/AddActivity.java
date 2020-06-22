7db3cc9c23663043a4c9b43984809c4c599dbf00
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-06-22 15:00:47 +0300

    Created the views needed and connected the project with a database. Created the add book view with 3 text fields and button with an onClick action. On the main page we also have a plus button which on click leads us to the add book view.

---
+++ app/src/main/java/com/example/booklist/AddActivity.java
@@ -0,0 +1,14 @@
+package com.example.booklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
